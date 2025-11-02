6ba3d3d85153a0f6dc8744698a0db403f550d39f
shyookjunk <shyook.junk@gmail.com>
2025-11-02 21:44:38 +0900

    start ch.9

--- dls5_genai/ch9/simple_unet.py
+++ dls5_genai/ch9/simple_unet.py
@@ -13,3 +13,24 @@ class ConvBlock(nn.Module):
                 nn.ReLU())
   def forward(self,x):
     return self.convs(x)
+  
+class UNet(nn.Module):
+  def __init__(self,in_ch=1):
+    super().__init__()
+    self.down1=ConvBlock(in_ch,64)
+    self.down2=ConvBlock(64,128)
+    self.bot2=ConvBlock(128,256)
+    self.up2=ConvBlock(128+256,128)
+    self.up1=ConvBlock(128+64,64)
+    self.out=nn.Conv2d(64,in_ch,1)
+
+    self.maxpool=nn.MaxPool2d(2)
+    self.upsample=nn.Upsample(scale_factor=2,mode='bilinear')
+
+  def forward(self,x):
+    x1=self.down1(x)
+    x-self.maxplool(x1)
+    x2=self.down2(x)
+    x=self.maxplool(x2)
+    
+    x=self.bot1(x)
