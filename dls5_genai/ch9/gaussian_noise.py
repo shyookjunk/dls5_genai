493aa432ca438852998eca6be6c5e705458af5d5
shyookjunk <shyook.junk@gmail.com>
2025-11-07 10:24:14 +0900

    add add noise

--- dls5_genai/ch9/gaussian_noise.py
+++ dls5_genai/ch9/gaussian_noise.py
@@ -4,6 +4,13 @@ import torchvision.transforms as transforms
 import numpy as np
 import matplotlib.pyplot as plt
 
+def reverse_to_img(x):
+  x=x*255
+  x=x.clamp(0,255)
+  x=x.to(torch.uint8)
+  to_pil=transforms.ToPILImage()
+  return to_pil(x)
+
 # read image
 current_dir=os.path.dirname(os.path.abspath(__file__))
 file_path=os.path.join(current_dir,'flower.png')
@@ -14,6 +21,31 @@ print(image.shape)
 preprocess=transforms.ToTensor()
 x=preprocess(image)
 print(x.shape)
+
+T=1000
+beta_start=0.0001
+beta_end=0.02
+betas=torch.linspace(beta_start,beta_end,T)
+imgs=[]
+
+for t in range(T):
+  if t%100==0:
+    img=reverse_to_img(x)
+    imgs.append(img)
+
+  beta=betas[t]
+  eps=torch.randn_like(x)
+  x=torch.sqrt(1-beta)*x+torch.sqrt(beta)*eps
+
+plt.figure(figsize=(15,6))
+for i,img in enumerate(imgs[:10]):
+  plt.subplot(2,5,i+1)
+  plt.imshow(img)
+  plt.title(f'Noise: {i*100}')
+  plt.axis('off')
+
+plt.show()
+
 
 # x=torch.randn(3,64,64)
 # T=1000
