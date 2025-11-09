ce8c03484a7905f1fc900bb836bd272daee6ae63
shyookjunk <shyook.junk@gmail.com>
2025-11-09 21:12:27 +0900

    add conditional diffusion

--- dls5_genai/ch10/conditional.py
+++ dls5_genai/ch10/conditional.py
@@ -52,7 +52,7 @@ class ConvBlock(nn.Module):
     y=self.convs(x+v)
     return y
   
-  class UNet(nn.Module):
+class UNetCond(nn.Module):
   def __init__(self,in_ch=1,time_embed_dim=100,num_labels=None):
     super().__init__()
     self.time_embed_dim=time_embed_dim
@@ -70,21 +70,162 @@ class ConvBlock(nn.Module):
     if num_labels is not None:
       self.label_emb=nn.Embedding(num_labels,time_embed_dim)
 
-  def forward(self,x,timesteps):
-    v=pos_encoding(timesteps,self.time_embed_dim,x.device)
-
-    x1=self.down1(x,v)
+  def forward(self,x,timesteps,labels=None):
+    t=pos_encoding(timesteps,self.time_embed_dim,x.device)
+    
+    if labels is not None:
+      t+=self.label_emb(labels)
+    x1=self.down1(x,t)
     x=self.maxpool(x1)
-    x2=self.down2(x,v)
+    x2=self.down2(x,t)
     x=self.maxpool(x2)
 
-    x=self.bot1(x,v)
+    x=self.bot1(x,t)
 
     x=self.upsample(x)
     x=torch.cat([x,x2],dim=1)   # 스킵연결
-    x=self.up2(x,v)
+    x=self.up2(x,t)
     x=self.upsample(x)
     x=torch.cat([x,x1],dim=1)
-    x=self.up1(x,v)
+    x=self.up1(x,t)
     x=self.out(x)
     return x
+  
+class Diffuser():
+  def __init__(self,num_timesteps=1000,beta_start=0.0001,beta_end=0.02,device='cpu'):
+    self.num_timesteps=num_timesteps
+    self.device=device
+    self.betas=torch.linspace(beta_start,beta_end,num_timesteps,device=device)
+    self.alphas=1-self.betas
+    self.alpha_bars=torch.cumprod(self.alphas,dim=0)
+
+  def add_noise(self,x_0,t):
+    T=self.num_timesteps
+    assert (t>=1).all() and (t<=T).all()
+
+    t_idx=t-1
+
+    alpha_bar=self.alpha_bars[t_idx]
+    N=alpha_bar.size(0)
+    alpha_bar=alpha_bar.view(N,1,1,1)
+
+    noise=torch.randn_like(x_0,device=self.device)
+    x_t=torch.sqrt(alpha_bar)*x_0+torch.sqrt(1-alpha_bar)*noise
+
+    return x_t,noise
+
+  def denoise(self,model,x,t,labels):
+    T=self.num_timesteps
+    assert (t>=1).all() and (t<=T).all()
+
+    t_idx=t-1
+    alpha=self.alphas[t_idx]
+    alpha_bar=self.alpha_bars[t_idx]
+    alpha_bar_prev=self.alpha_bars[t_idx-1]
+
+    N=alpha.size(0)
+    alpha=alpha.view(N,1,1,1)
+    alpha_bar=alpha_bar.view(N,1,1,1)
+    alpha_bar_prev=alpha_bar_prev.view(N,1,1,1)
+
+    model.eval()
+    with torch.no_grad():
+      eps=model(x,t,labels)
+    model.train()
+
+    noise=torch.randn_like(x,device=self.device)
+    noise[t==1]=0
+
+    mu=(x-((1-alpha)/torch.sqrt(1-alpha_bar))*eps)/torch.sqrt(alpha)
+    std=torch.sqrt((1-alpha)*(1-alpha_bar_prev)/(1-alpha_bar))
+
+    return mu+noise*std
+  
+  def reverse_to_img(self,x):
+    x=x*255
+    x=x.clamp(0,255)
+    x=x.to(torch.uint8)
+    x=x.cpu()
+    to_pil=transforms.ToPILImage()
+    return to_pil(x)
+  
+  def sample(self,model,x_shape=(20,1,28,28),labels=None):
+    batch_size=x_shape[0]
+    x=torch.randn(x_shape,device=self.device)
+
+    if labels is None:
+      labels=torch.randint(0,10,(len(x),),device=self.device)
+
+    for i in tqdm(range(self.num_timesteps,0,-1)):
+      t=torch.tensor([i]*batch_size,device=self.device,dtype=torch.long)
+      x=self.denoise(model,x,t,labels)
+
+    images=[self.reverse_to_img(x[i]) for i in range(batch_size)]
+    return images,labels
+
+def show_images(images,rows=2,cols=10,labels=None):
+  fig=plt.figure(figsize=(cols,rows))
+  i=0
+  for r in range(rows):
+    for c in range(cols):
+      fig.add_subplot(rows,cols,i+1)
+      plt.imshow(images[i],cmap='gray')
+      plt.axis('off')
+      if labels is not None:
+        plt.title(str(labels[i]),fontsize=10)
+      i+=1
+  plt.show()
+
+img_size=28
+batch_size=128
+num_timesteps=1000
+epochs=10
+lr=1e-3
+device='cuda' if torch.cuda.is_available() else 'cpu'
+print("Device=",device)
+
+preprocess=transforms.ToTensor()
+dataset=torchvision.datasets.MNIST(root="./data",download=True,transform=preprocess)
+dataloader=DataLoader(dataset,batch_size=batch_size,shuffle=True)
+
+diffuser=Diffuser(num_timesteps,device=device)
+model=UNetCond(num_labels=10)
+model.to(device)
+optimizer=Adam(model.parameters(),lr=lr)
+
+losses=[]
+for epoch in range(epochs):
+  loss_sum=0.0
+  cnt=0
+
+  for images,labels in tqdm(dataloader):
+    optimizer.zero_grad()
+    x=images.to(device)
+    labels=labels.to(device)
+    t=torch.randint(1,num_timesteps+1,(len(x),),device=device)
+
+    x_noisy,noise=diffuser.add_noise(x,t)
+    noise_pred=model(x_noisy,t,labels)
+    loss=F.mse_loss(noise,noise_pred)
+
+    loss.backward()
+    optimizer.step()
+
+    loss_sum+=loss.item()
+    cnt+=1
+
+  loss_avg=loss_sum/cnt
+  losses.append(loss_avg)
+  print(f'Epoch {epoch} | Loss: {loss_avg}')
+
+# plt.plot(losses)
+# plt.xlabel('Epoch')
+# plt.ylabel('Loss')
+# plt.show()
+
+images,labels=diffuser.sample(model)
+labels=labels.to('cpu')
+print("labels=",labels)
+labels=labels.numpy()
+print(">> labels=",labels)
+show_images(images,labels=labels)
